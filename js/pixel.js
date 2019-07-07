d24bdd2d83a5e699229c94ac169d716297222e8d
Zoli Szogyenyi <zoltan.szogyenyi@gmail.com>
2019-07-07 14:14:59 +0300

    Initial commit.

---
+++ js/pixel.js
@@ -0,0 +1,168 @@
+/*!
+
+=========================================================
+* Pixel Bootstrap 4 UI Kit
+=========================================================
+
+* Product Page: http://pixel.themesberg.com
+* Copyright 2018 Themesberg (https://www.themesberg.com)
+
+* Coded by themesberg.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+
+"use strict";
+$(document).ready(function () {
+
+    var $navbarCollapse = $('.navbar-main .collapse');
+
+    // Collapse navigation
+    $navbarCollapse.on('hide.bs.collapse', function () {
+        var $this = $(this);
+        $this.addClass('collapsing-out');
+        $('html, body').css('overflow', 'initial');
+    });
+
+    $navbarCollapse.on('hidden.bs.collapse', function () {
+        var $this = $(this);
+        $this.removeClass('collapsing-out');
+    });
+
+    $navbarCollapse.on('shown.bs.collapse', function () {
+        $('html, body').css('overflow', 'hidden');
+    });
+
+    $('.navbar-main .dropdown').on('hide.bs.dropdown', function () {
+        var $this = $(this).find('.dropdown-menu');
+
+        $this.addClass('close');
+
+        setTimeout(function () {
+            $this.removeClass('close');
+        }, 200);
+
+    });
+
+    // Headroom - show/hide navbar on scroll
+    if ($('.headroom')[0]) {
+        var headroom = new Headroom(document.querySelector("#navbar-main"), {
+            offset: 0,
+            tolerance: {
+                up: 0,
+                down: 0
+            },
+        });
+        headroom.init();
+    }
+
+    // Background images for sections
+    $('[data-background]').each(function () {
+        $(this).css('background-image', 'url(' + $(this).attr('data-background') + ')');
+    });
+
+    $('[data-background-color]').each(function () {
+        $(this).css('background-color', $(this).attr('data-background-color'));
+    });
+
+    $('[data-color]').each(function () {
+        $(this).css('color', $(this).attr('data-color'));
+    });
+
+    // Tooltip
+    $('[data-toggle="tooltip"]').tooltip();
+
+    // Popover
+    $('[data-toggle="popover"]').each(function () {
+        var popoverClass = '';
+        if ($(this).data('color')) {
+            popoverClass = 'popover-' + $(this).data('color');
+        }
+        $(this).popover({
+            trigger: 'focus',
+            template: '<div class="popover ' + popoverClass + '" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
+        })
+    });
+
+    // Additional .focus class on form-groups
+    $('.form-control').on('focus blur', function (e) {
+        $(this).parents('.form-group').toggleClass('focused', (e.type === 'focus' || this.value.length > 0));
+    }).trigger('blur');
+
+    $(".progress-bar").each(function () {
+        $(this).waypoint(function () {
+            var progressBar = $(".progress-bar");
+            progressBar.each(function (indx) {
+                $(this).css("width", $(this).attr("aria-valuenow") + "%");
+            });
+            $('.progress-bar').css({
+                animation: "animate-positive 3s",
+                opacity: "1"
+            });
+        }, {
+                triggerOnce: true,
+                offset: '90%'
+            });
+    });
+
+    // When in viewport
+    $('[data-toggle="on-screen"]')[0] && $('[data-toggle="on-screen"]').onScreen({
+        container: window,
+        direction: 'vertical',
+        doIn: function () {
+            //alert();
+        },
+        doOut: function () {
+            // Do something to the matched elements as they get off scren
+        },
+        tolerance: 200,
+        throttle: 50,
+        toggleClass: 'on-screen',
+        debug: false
+    });
+
+    // Scroll to anchor with scroll animation
+    $('[data-toggle="scroll"]').on('click', function (event) {
+        var hash = $(this).attr('href');
+        var offset = $(this).data('offset') ? $(this).data('offset') : 0;
+
+        // Animate scroll to the selected section
+        $('html, body').stop(true, true).animate({
+            scrollTop: $(hash).offset().top - offset
+        }, 600);
+
+        event.preventDefault();
+    });
+
+    //Smooth scroll
+    var scroll = new SmoothScroll('a[href*="#"]', {
+        speed: 500,
+        speedAsDuration: true
+    });
+
+    // copy docs
+    $('.copy-docs').on('click', function () {
+        var $copy = $(this);
+        var htmlEntities = $copy.parents('.nav-wrapper').siblings('.card').find('.tab-pane:last-of-type').html();
+        var htmlDecoded = $('<div/>').html(htmlEntities).text().trim();
+
+        var $temp = $('<textarea>');
+        $('body').append($temp);
+        console.log(htmlDecoded);
+        $temp.val(htmlDecoded).select();
+        document.execCommand('copy');
+        $temp.remove();
+
+        $copy.text('Copied!');
+        $copy.addClass('copied');
+
+        setTimeout(function () {
+            $copy.text('Copy');
+            $copy.removeClass('copied');
+        }, 1000);
+    });
+
+});   
